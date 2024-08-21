61757755603b88756a55f2d5acf530dd968e3324
Acacio S. Costa <acaciosantossi@gmail.com>
2024-08-21 16:09:33 -0300

    splash screen

--- App.tsx
+++ App.tsx
@@ -3,15 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/routes';
 import { initializeStorage } from './services/storageService';
 import { mmkvStorage } from './services/mmkvStorage';
-import { asyncStorage } from './services/asyncStorage';
 import AppProviders from './src/contexts/Contexts';
+import BootSplash from "react-native-bootsplash";
 
 initializeStorage(mmkvStorage)
 
 export default function App() {
   return (
     <AppProviders>
-      <NavigationContainer>
+      <NavigationContainer
+        onReady={() => BootSplash.hide({ fade: true })}
+      >
         <StackNavigator />
       </NavigationContainer>
     </AppProviders>
